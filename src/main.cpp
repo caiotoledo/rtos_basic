bd144f89ac1198646363b9222e67f439841ff1fa
Caio Toledo <caio_toledo_2@hotmail.com>
2018-01-11 22:09:22 -0200

    Adding loopback from USB to serial, for testing.

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,17 @@
 #define BLINK_TIME    500
 
 DigitalOut led1(LED1);
+Serial serial(SERIAL_TX, SERIAL_RX, 9600);
 
 Thread tLED;
 Thread tImu(osPriorityRealtime,DEFAULT_STACK_SIZE,0);
+
+void data_recv(void){
+  char *buf = (char *) malloc( sizeof(char) * (pc.available() + 1) );
+  pc.gets(buf, pc.available()+1);
+  serial.printf("%s", buf);
+  free(buf);
+}
 
 void led_thread(void){
   while(true){
@@ -20,11 +28,13 @@ int main() {
   char ver[50];
   formatVersion(ver);
 
+  pc.attach(data_recv);
+
   tImu.start(imu_thread);
   tLED.start(led_thread);
 
   while (true) {
     pc.printf("Version %s %s\r\n", PROJECT_NAME, ver);
-    Thread::wait(5000);
+    Thread::wait(20000);
   }
 }
